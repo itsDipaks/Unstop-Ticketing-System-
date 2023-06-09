e4f6f1bc0e36c0a542098bda539abb22ead995d0
Dipak Pawar <itsdipakpawar4206@gmail.com>
2023-06-09 15:47:30 +0530

    Index File Updated

--- Backend/index.js
+++ Backend/index.js
@@ -7,25 +7,6 @@ const app=express()
 
 app.use(express.json())
 
-const seats = [];
-for (let i = 1; i <= 80; i++) {
-  seats.push({
-    SeatNumber: i,
-    Isbooked: false,
-    BookinTime: Date.now(),
-  });
-}
-
-SeatModel.insertMany(seats)
-  .then(() => {
-    console.log("Seats added to the database successfully!");
-    mongoose.disconnect();
-  })
-  .catch((error) => {
-    console.error("Error adding seats to the database:", error);
-    mongoose.disconnect();
-  });
-
 app.listen(8400,async()=>{
     try{
     console.log("Server Started At http://localhost:8400")
